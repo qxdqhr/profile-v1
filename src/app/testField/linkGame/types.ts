f9de6d1e27009d69c404939ec9503ed2e7dac3b5
qihongrui <>
2025-01-27 00:41:43 +0800

    111

--- src/app/testField/linkGame/types.ts
+++ src/app/testField/linkGame/types.ts
@@ -25,9 +25,9 @@ export type GameStatus = 'playing' | 'success' | 'failed'
 export type GameType = 'disvariable' | 'downfalling'
 
 // 游戏常量
-export const GRID_SIZE = 8
+export const GRID_SIZE = 10
 export const TILE_SIZE = 70
 export const TILE_GAP = 10
-export const TYPES_COUNT = 10
+export const TYPES_COUNT = 30
 export const OUTER_PADDING = 1
 export const GAME_DURATION = 300 // 游戏时长（秒） 
