fdf62a376d66a985b0f78e4085867a48b1b6ca0a
qihongrui <>
2025-05-02 00:05:19 +0800

    no message

--- src/app/(pages)/testField/experiment/components/index.ts
+++ src/app/(pages)/testField/experiment/components/index.ts
@@ -1,8 +1,5 @@
-export { default as StatusBar } from './StatusBar';
 export { default as QuestionContent } from './QuestionContent';
 export { default as OptionsList } from './OptionsList';
-export { default as Navigation } from './Navigation';
+export { default as NavigationControls } from './NavigationControls';
 export { default as QuestionTypeTag } from './QuestionTypeTag';
-export { default as QuestionList } from './QuestionList';
 export { default as QuestionHeader } from './QuestionHeader';
-export { default as Question } from './Question'; 
