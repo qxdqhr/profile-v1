07c74d8908b6df01c4caceeb8a6116659c4a83d4
qihongrui <>
2025-05-14 23:16:03 +0800

    no message

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,7 +5,7 @@ import * as schema from './schema';
 import 'dotenv/config';
 
 // 根据环境获取数据库连接URL
-const connectionString = process.env.DEVELOPMENT_DATABASE_URL;
+const connectionString = process.env.DATABASE_URL;
 
 // 检查 connectionString 是否存在
 if (!connectionString) {
